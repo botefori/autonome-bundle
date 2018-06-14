5170270b89ec5acc0edd8d1ef94e98f03f108992
Ibrahima <icamara@wynd.eu>
2018-06-14 12:39:49 +0200

    Create word provider interface

--- spaceBar/lib/LoremIpsumBundle/src/KnpUWordProvider.php
+++ spaceBar/lib/LoremIpsumBundle/src/KnpUWordProvider.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace KnpU\LoremIpsumBundle;
 
 
-class KnpUWordProvider
+class KnpUWordProvider implements WordProviderInterface
 {
   public function getWordList():array
   {
